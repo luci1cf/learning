3a0ef7c2f69e169550e08e5263dc57dbdcf6741e
ic-luc <flucic097@gmail.com>
2026-03-02 17:23:56 +0100

    ep-9: faster way of decreasing the value of a variable by 1

--- java-by-thenewboston/ep-9/src/Main.java
+++ java-by-thenewboston/ep-9/src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int tuna = 5;
         int bass = 18;
-        System.out.println(tuna++);
-        System.out.println(tuna);
+        System.out.println(--tuna);
     }
 }
