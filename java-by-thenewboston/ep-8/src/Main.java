dfe56b7e37eb3117814643a3f9a6b9aac0e97f0c
ic-luc <flucic097@gmail.com>
2026-03-02 17:11:20 +0100

    ep-8: example of subtraction in java

--- java-by-thenewboston/ep-8/src/Main.java
+++ java-by-thenewboston/ep-8/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int girls, boys, people;
         girls = 6;
         boys = 3;
-        people = girls + boys;
+        people = girls - boys;
 
         System.out.println(people);
     }
