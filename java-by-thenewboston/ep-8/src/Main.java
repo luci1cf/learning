da50a1f46013ba0493d9a814ba37904f36c40b0a
ic-luc <flucic097@gmail.com>
2026-03-02 17:08:17 +0100

    ep-8: declared the values and assigned a value to each one of them

--- java-by-thenewboston/ep-8/src/Main.java
+++ java-by-thenewboston/ep-8/src/Main.java
@@ -3,5 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner bucky = new Scanner(System.in);
+
+        int girls, boys, people;
+        girls = 6;
+        boys = 3;
+        people = girls + boys;
     }
 }
