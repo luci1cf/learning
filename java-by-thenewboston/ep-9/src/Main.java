4b4986b5192abcfa98519459a19bcc5555240450
ic-luc <flucic097@gmail.com>
2026-03-02 17:20:46 +0100

    ep-9: got rid of the redundant code (wrote the increment directly in the output line)

--- java-by-thenewboston/ep-9/src/Main.java
+++ java-by-thenewboston/ep-9/src/Main.java
@@ -2,8 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int tuna = 5;
         int bass = 18;
-
-        ++tuna;
-        System.out.println(tuna);
+        System.out.println(++tuna);
     }
 }
