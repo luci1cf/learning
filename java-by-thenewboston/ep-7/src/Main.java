ef6e5adb81c4e5fb5bb07297308f5fb674f980c2
ic-luc <flucic097@gmail.com>
2026-03-02 16:59:25 +0100

    ep-7: defined the variables fnum & snum where we will store the inputs

--- java-by-thenewboston/ep-7/src/Main.java
+++ java-by-thenewboston/ep-7/src/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner bucky = new Scanner(System.in);
         double fnum, snum, answer;
+
+        System.out.println("Enter first num: ");
+        fnum = bucky.nextDouble();
+
+        System.out.println("Enter second num: ");
+        snum = bucky.nextDouble();
     }
 }
