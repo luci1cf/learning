7bf2e0c3dcb25a6bb81a40283d736f7ec7615309
ic-luc <flucic097@gmail.com>
2026-03-02 12:47:27 +0100

    ep-7: created a new scanner so we can get user input and initialised three variables

--- java-by-thenewboston/ep-7/src/Main.java
+++ java-by-thenewboston/ep-7/src/Main.java
@@ -1,2 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner bucky = new Scanner(System.in);
+        double fnum, snum, answer;
+    }
 }
