95aa6db740478b3589e87119e98b3cd4b7ca99eb
ic-luc <flucic097@gmail.com>
2026-03-03 08:12:54 +0100

    ep-10: if-statement for value comparison (is one value smaller than the other one?)

--- java-by-thenewboston/ep-10/src/Main.java
+++ java-by-thenewboston/ep-10/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int test = 6;
 
-        if (test == 9) {
+        if (test < 9) {
             System.out.println("Yes.");
         } else {
             System.out.println("No.");
