3b455a9b020cf084d5fc13c43e719d5594de3d14
ic-luc <flucic097@gmail.com>
2026-03-03 08:25:39 +0100

    ep-11: changed the values of both values to see how the output would change (neither argument was fulfilled -> it goes to the else-statement)

--- java-by-thenewboston/ep-11/src/Main.java
+++ java-by-thenewboston/ep-11/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int boy, girl;
-        boy = 18;
-        girl = 40;
+        boy = 8;
+        girl = 99;
 
         if (boy > 10 || girl < 60) {
             System.out.println("You can enter");
