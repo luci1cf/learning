f7b37b1f1a190dc5cadc4e80c4261f2b0107c7be
ic-luc <flucic097@gmail.com>
2026-03-03 08:09:37 +0100

    ep-10: a simple example of an if-statement in java

--- java-by-thenewboston/ep-10/src/Main.java
+++ java-by-thenewboston/ep-10/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        int test = 6;
 
+        if (test == 9) {
+            System.out.println("Yes.");
+        } else {
+            System.out.println("No.");
+        }
     }
 }
