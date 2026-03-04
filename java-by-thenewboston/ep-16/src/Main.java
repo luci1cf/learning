ca3091d65f457a76e11d165165fd4701832228a1
ic-luc <flucic097@gmail.com>
2026-03-04 13:57:54 +0100

    ep-16: now the user input will be assigned as a value to variable 'girlName' and written out in the console afterwards

--- java-by-thenewboston/ep-16/src/Main.java
+++ java-by-thenewboston/ep-16/src/Main.java
@@ -7,5 +7,8 @@ public class Main {
 
         System.out.println("Enter name of first gf here: ");
         String temp = input.nextLine();
+
+        tunaObject.setGirlName(temp);
+        tunaObject.saying();
     }
 }
