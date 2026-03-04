48da91e8c016201b1b544d253aafa3bfaf8248c0
ic-luc <flucic097@gmail.com>
2026-03-04 13:53:12 +0100

    ep-16: wrote the getter and setter method for the variable 'girlName'

--- java-by-thenewboston/ep-16/src/Tuna.java
+++ java-by-thenewboston/ep-16/src/Tuna.java
@@ -1,3 +1,11 @@
 public class Tuna {
     private String girlName;
+
+    public void setGirlName(String girlName) {
+        this.girlName = girlName;
+    }
+
+    public String getGirlName() {
+        return girlName;
+    }
 }
