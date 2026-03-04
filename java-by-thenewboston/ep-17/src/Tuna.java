954c8e7a9e34b0fefc989639ad0edd187ad27a91
ic-luc <flucic097@gmail.com>
2026-03-04 14:01:33 +0100

    ep-17: added the code from previous project 'ep-16'

--- java-by-thenewboston/ep-17/src/Tuna.java
+++ java-by-thenewboston/ep-17/src/Tuna.java
@@ -1,2 +1,15 @@
 public class Tuna {
+    private String girlName;
+
+    public String getGirlName() {
+        return girlName;
+    }
+
+    public void setGirlName(String girlName) {
+        this.girlName = girlName;
+    }
+
+    public void saying() {
+        System.out.println("Your first gf was %s\n", getGirlName());
+    }
 }
