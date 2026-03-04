08bfe0b157e201a20f76d85cec214a7212dea969
ic-luc <flucic097@gmail.com>
2026-03-04 13:45:31 +0100

    ep-15: imported the method from 'Tuna' class and wrote the code for user input

--- java-by-thenewboston/ep-15/src/Main.java
+++ java-by-thenewboston/ep-15/src/Main.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Tuna tunaObject = new Tuna();
+
+        System.out.println("Enter your name here: ");
+        String name = input.nextLine();
     }
 }
