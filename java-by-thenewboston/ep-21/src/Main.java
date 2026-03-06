870d56346ed5d6e91d2a7e6dcc28acea35d7065e
ic-luc <flucic097@gmail.com>
2026-03-06 10:22:51 +0100

    ep-21: declared variables necessary for the calculation

--- java-by-thenewboston/ep-21/src/Main.java
+++ java-by-thenewboston/ep-21/src/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        int total = 0;
+        int grade;
+        int average;
+        int counter = 0;
     }
 }
