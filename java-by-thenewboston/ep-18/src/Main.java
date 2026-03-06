a6033c04328d6bd58e23dcbc43d9dbc401083cfb
ic-luc <flucic097@gmail.com>
2026-03-06 10:03:06 +0100

    ep-18: changed the value of variable 'age' & nested an if-else block inside of an if-else block (into the else statement to be precise)

--- java-by-thenewboston/ep-18/src/Main.java
+++ java-by-thenewboston/ep-18/src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 50;
+        int age = 60;
 
         if (age < 50) {
             System.out.println("You are young");
         } else {
             System.out.println("You are old");
+
+            if (age > 75) {
+                System.out.println("You are REALLY old");
+            } else {
+                System.out.println("Don't worry, you aren't really that old");
+            }
         }
     }
 }
