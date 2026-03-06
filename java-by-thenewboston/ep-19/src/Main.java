8d04f2f461402e58eb768f2ab7bea16b5808967d
ic-luc <flucic097@gmail.com>
2026-03-06 10:11:17 +0100

    ep-19: declared a value 'age', assigned a value to it & wrote a simple if-else block based on that variable

--- java-by-thenewboston/ep-19/src/Main.java
+++ java-by-thenewboston/ep-19/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        int age = 55;
 
+        if (age > 60) {
+            System.out.println("You are a senior citizen");
+        } else {
+            System.out.println("You are a young buck");
+        }
     }
 }
