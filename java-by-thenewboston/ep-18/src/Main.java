a8d3616d9652d5d5c474b88b0540de9e5b2fe00e
ic-luc <flucic097@gmail.com>
2026-03-06 09:59:57 +0100

    ep-18: wrote a simple example of an if-else statement block

--- java-by-thenewboston/ep-18/src/Main.java
+++ java-by-thenewboston/ep-18/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int age = 50;
+
+        if (age < 50) {
+            System.out.println("You are young");
+        } else {
+            System.out.println("You are old");
+        }
     }
 }
