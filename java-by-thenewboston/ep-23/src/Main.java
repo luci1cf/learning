139437fba6e6e52a2cb7eab86f66d689de0e0055
ic-luc <flucic097@gmail.com>
2026-03-17 15:47:42 +0100

    ep-23: made a simple compound interest program, which shows how the initial value changes daily under influence of a certain interest rate

--- java-by-thenewboston/ep-23/src/Main.java
+++ java-by-thenewboston/ep-23/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        // A = P(1+R)^n - the formula for our compound interest program
 
+        double amount;
+        double principal = 10000;
+        double rate = 0.01;
+
+        for (int day = 1; day <= 20; day++) {
+            amount = principal*Math.pow(1 + rate, day);
+            System.out.println(day + "    " + amount);
+        }
     }
 }
