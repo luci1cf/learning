ffc5db7d1687ca76dfdabdb5f18c7bacea807f33
ic-luc <flucic097@gmail.com>
2026-03-17 15:40:22 +0100

    ep-22: wrote a simple for loop which prints out number 1 - 10 in the console

--- java-by-thenewboston/ep-22/src/Main.java
+++ java-by-thenewboston/ep-22/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        for (int counter = 1; counter <= 10; counter++) {
+            System.out.println(counter);
+        }
     }
 }
