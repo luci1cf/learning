b75f952fa08194a75bdab651bb89fa999ceab9ab
ic-luc <flucic097@gmail.com>
2026-03-17 15:56:32 +0100

    ep-25: wrote some basic 'Math' class functions

--- java-by-thenewboston/ep-25/src/Main.java
+++ java-by-thenewboston/ep-25/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Math.abs(-26.7));
+        System.out.println(Math.ceil(7.4));
+        System.out.println(Math.floor(7.8));
+        System.out.println(Math.max(8.6, 5.2));
+        System.out.println(Math.min(8.6, 5.2));
+        System.out.println(Math.min(5, 3));
+        System.out.println(Math.sqrt(9));
     }
 }
