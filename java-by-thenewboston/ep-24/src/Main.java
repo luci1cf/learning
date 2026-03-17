90115ca172eaa469e5cc577b8d39fe12dd389e80
ic-luc <flucic097@gmail.com>
2026-03-17 15:50:04 +0100

    ep-24: a small example of how do-while loops work

--- java-by-thenewboston/ep-24/src/Main.java
+++ java-by-thenewboston/ep-24/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int counter = 0;
 
+        do {
+            System.out.println(counter);
+            counter++;
+        } while (counter <= 10);
     }
 }
